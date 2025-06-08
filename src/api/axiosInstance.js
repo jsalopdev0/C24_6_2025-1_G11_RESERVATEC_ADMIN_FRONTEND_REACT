f6361d5210a3c07acd1190c502cfce509d6fd7e7
jsalopdev0 <jsalopdev@gmail.com>
2025-06-08 11:45:58 -0500

    cors

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://reservatec-tesis-backend-8asuen-298379-31-220-104-112.traefik.me/api',
-
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://reservatec-tesis-backend-8asuen-298379-31-220-104-112.traefik.me/api',
   headers: {
     'Content-Type': 'application/json',
   },
